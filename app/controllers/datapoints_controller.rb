46e5229428f7df1c29d17b88e62bc6fbb6487c04
alexanderkjones <alexanderkjones@gmail.com>
2013-04-19 11:30:32 -0400

    Last commit

--- app/controllers/datapoints_controller.rb
+++ app/controllers/datapoints_controller.rb
@@ -2,40 +2,63 @@ class DatapointsController < ApplicationController
 	
 	# GET /datapoints
   	# GET /datapoints.json
-  	# Show last 10 Datapoints
 	def index
-		#connect to DB
+		# connect to DB
 		db = AWS::DynamoDB.new
 		table = db.tables['hive_data_store']
 		table.hash_key = [:hive_id, :string]
 		table.range_key = [:date_time, :number]
 
+		# initialize data array
 		datapoints = []
 
 		# request a maximum of 10 items from Amazon DynamoDB
 		table.items.select(:limit => 10).each do |item_data|
 			datapoints.push(item_data.attributes)
 		end
-		# Build Index
-		@var = datapoints
-
 		
-
+		# pass datapoints to /index
+		@var = datapoints
 	end
 
 	# POST /datapoints
   	# POST /datapoints.json
 	def create
-		#connect to DB
-		/db = AWS::DynamoDB.new
+		# connect to DB
+		db = AWS::DynamoDB.new
 		table = db.tables['hive_data_store']
 		table.hash_key = [:hive_id, :string]
-		table.range_key = [:date_time, :number]/
+		table.range_key = [:date_time, :number]
 
-		# adding one item at a time to the batch
-		#batch = AWS::DynamoDB::BatchWrite.new
+		# initialize batch
+		batch = AWS::DynamoDB::BatchWrite.new
+
+		# batch test
+		/batch.put(table, [
+			{:hive_id => 'HID100', :date_time => 2322323232, :color => 'green'}
+		])
+		batch.process!/
 
 		@dataset = params[:value]
+
+		@parsed_json = ActiveSupport::JSON.decode(@dataset)
+
+		my_id = @parsed_json["hive_id"];
+		my_time = @parsed_json["date_time"];
+
+		@parsed_json["data"].each do |datapoint|
+			dataHash = Hash[:hive_id => my_id, :date_time => my_time];
+			datapoint.each_pair do |k,v|
+				dataHash.merge!(k => v);
+			end
+			my_time = my_time -1;
+			File.open('test.txt', 'a') {|f| f.write(batch.inspect)};
+			thisa = Array.new();
+			thisa.push(dataHash)
+			batch.put(table, [dataHash]);
+		end
+		batch.process!
+
 
 		/@dataset[:datapoints].each do |datapoint|
 			@data = {
@@ -50,7 +73,7 @@ class DatapointsController < ApplicationController
 
 		batch.process!/
 
-		File.open('test.txt', 'a') {|f| f.write(@dataset) }
+		#File.open('test.txt', 'a') {|f| f.write(@parsed_json["data"]) }
 
 		
 	
